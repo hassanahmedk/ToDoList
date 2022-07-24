85682851b8c054fa0ca088aef4920c3b62f10861
Hassan <mr.und3ad1@gmail.com>
2022-07-24 09:45:49 -0700

    Added Login System, connected mongodb, landing page ready, can add listings

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const app = express();
 
 const db = require(__dirname + "/db");
 
+const mongoose = require("mongoose");
 
-let todoItems = ["Do  1", "Do  2", "Do  3"];
+mongoose.connect('mongodb://localhost:27017/ToDoListDB');
+
+//let todoItems = ["Do  1", "Do  2", "Do  3", "Test", "Test", "Test", "Test", "Test", "Test", "Test", "Test"];
+// let todoItems = ["Do  1", "Do  2", "Do  3"];
 let todoItemsWork = ["Work 1"];
 let loggedIn = false;
+let loggedInUser = "Hassan";
 
 let usernames = ["und", "hb", "admin"];
 let passwords = ["123", "pgi1", "wowowo"];
@@ -18,12 +23,41 @@ app.use(express.static(__dirname));
 
 app.use(express.urlencoded({extended:true}));
 
+
+// const userSchema = new mongoose.Schema({
+//     _id: String,
+//     FirstName: String,
+//     LastName: String,
+//     Email: String,
+//     Gender: String,
+//     Password: String
+// });
+
+
+// const user = mongoose.model('User', userSchema);
+
+
+
 db.initiateDB();
 
 
 app.get("/", function(req,res){
+
     res.render("login");
 });
+
+
+
+
+app.get("/index", function(req,res){
+
+    db.findAllItems()
+    .then(function(todoItems){
+        res.render("index", {userName: loggedInUser, items: todoItems} );
+    })
+
+});
+
 
 
 // app.get("/", function(req,res){
@@ -61,19 +95,79 @@ app.get("/", function(req,res){
 // });
 
 
-app.post("/", async function(req,res){
-    db.login(req.body.username, req.body.password)
-        .then(function(access){
-            if (access === true){
-                console.log("done");
+app.post("/", function(req,res){
+
+
+    let username = req.body.username;
+    let password = req.body.password;
+
+    // db.login(username, password)
+    //     .then(function(access){
+    //         if (access === true){
+    //             loggedInUser = name;
+    //             res.redirect("index");
+    //         } else {
+    //             console.log("No");
+    //         }
+    //     })
+
+    // if(db.login(username, password) === true){
+    //     loggedInUser = name;
+    //     res.redirect("index");
+    // } else {
+    //     console.log("No");
+    // }
+
+    db.getUserId(username)
+        .then(function(foundUser){
+            if(foundUser === null){
+                console.log("User doesn't exist");
+                res.redirect("/");
             } else {
-                console.log("No");
+                if(foundUser.Password === password){
+                    console.log("Access Granted");
+                    console.log(foundUser);
+                    loggedInUser = foundUser.FirstName;
+                    res.redirect("/index");
+                } else {
+                    console.log("Incorrect Password");
+                    res.redirect("/");
+                }
             }
         })
+    // db.user.findById(username, 'Password', function (err, foundUser) {
+    //     if(!err){
+
+    //         if(foundUser === null){
+    //             console.log("User doesn't exist");
+    //             res.render("/");
+    //         } else {
+    //             if(foundUser.Password === password){
+    //                 console.log("Access Granted");
+    //                 res.render("/index");
+    //             } else {
+    //                 console.log("Incorrect Password");
+    //                 res.render("/");
+    //             }
+    //         }
+    //     }
+    // });
     
 });
 
+app.post("/addItem", function(req,res){
+    db.addItem(req.body.newItem);
+    res.redirect("/index");
+});
 
+app.post("/changeFavorite", function(req,res){
+    console.log(req.body);
+    db.changeFavorite(req.body.id, req.body.favoriteStatus)
+    .then(function(){
+        res.redirect("/index");
+
+    })
+});
 
 
 app.listen(3000, function(){
--- db.js
+++ db.js
@@ -4,92 +4,258 @@ const mongoose = require("mongoose");
 mongoose.connect('mongodb://localhost:27017/ToDoListDB');
 
 const userSchema = new mongoose.Schema({
+    _id: String,
     FirstName: String,
     LastName: String,
-    Username: String,
     Email: String,
     Gender: String,
     Password: String
 });
 
 
+const itemSchema = new mongoose.Schema({
+    data: String,
+    important: Boolean
+});
+
+const listSchema = new mongoose.Schema({
+    name: String,
+    items: [itemSchema]
+});
+
+
 const user = mongoose.model('User', userSchema);
+
+const item = mongoose.model('Item', itemSchema);
+
+const list = mongoose.model('List', listSchema);
 
 
 module.exports.initiateDB = function(){
-    const user1 = new user({
-        FirstName: "Hassan",
-        LastName: "Ahmed",
-        Username: "mrund3ad1",
-        Email: "mr.und3ad1@gmail.com",
-        Gender: "Male",
-        Password: "wowowo"
+    // will add default users only for the first time
+    user.find({}, function(err, foundUsers){
+            if(foundUsers.length === 0){
+                const user1 = new user({
+                    _id: "mrund3ad1",
+                    FirstName: "Hassan",
+                    LastName: "Ahmed",
+                    Email: "mr.und3ad1@gmail.com",
+                    Gender: "Male",
+                    Password: "wowowo"
+                })
+            
+                const user2 = new user({
+                    _id: "hbeasty",
+                    FirstName: "Hassan",
+                    LastName: "Khan",
+                    Email: "ihassanahmedkhan@gmail.com",
+                    Gender: "Male",
+                    Password: "hbeasty"
+                })
+            
+            
+                user.insertMany([user1, user2], function(e){
+                    if(e){
+                        console.log(e);
+                    } else {
+                        console.log("Added Users");
+                    }
+                })
+        }
+    })
+    
+    const item1 = new item({
+        data: "To Do Item 1",
+        important: false
+    })
+    const item2 = new item({
+        data: "To Do Item 2",
+        important: true
+    })
+    const item3 = new item({
+        data: "To Do Item 3",
+        important: false
     })
 
-    const user2 = new user({
-        FirstName: "Hassan",
-        LastName: "Khan",
-        Username: "hbeasty",
-        Email: "ihassanahmedkhan@gmail.com",
-        Gender: "Male",
-        Password: "hbeasty"
-    })
+    const defaultItems = [item1, item2, item3]; 
 
-
-    // user.insertMany([user1, user2], function(e){
-    //     if(e){
-    //         console.log(e);
-    //     } else {
-    //         console.log("Added Users");
+    // item.find({}, function(err, items){
+    //         if(items.length === 0){
+    //             item.insertMany(defaultItems, function(e){
+    //                 if(e){
+    //                     console.log(e);
+    //                 } else {
+    //                     console.log("Added Items");
+    //                 }
+    //             });
     //     }
     // })
+
+    list.find({}, function(err, lists){
+        if(lists.length === 0){
+            const defaultList = {
+                name: "My Day",
+                items: defaultItems
+            }
+
+            list.insertMany([defaultList], function(e){
+                if(e)   console.log(e);
+                else    console.log("added list");
+            })
+        }
+    })
 }
 
 
 
 
-// find with user provided username
+// **** find with user provided username
 
-findAll = async function(userUsername, userPassword){
+module.exports.findAllUsers = async function(){
 
     const result = await user.find({});
     return result;
 
+}
+module.exports.findAllItems = async function(){
+
+    const result = await item.find({});
+    return result;
+
+}
+
+module.exports.changeFavorite = async function(id, status){
+    console.log(id + "   " + status);
+    if(status==="false"){
+        item.updateOne({_id: id}, {important:true}, function(err, result){
+            if(!err){
+                console.log(result);
+            } else {
+                return err;
+            }
+        });
+    } else {
+        item.updateOne({_id: id}, {important:false}, function(err, result){
+            if(!err){
+                console.log(result);
+            } else {
+                return err;
+            }
+        });
+    }
 
 }
 
 
+
+module.exports.getUserId = async function(username){
+    let result = await user.findById(username);
+    return result;
+}
+
+module.exports.addItem =  function(newItemData){
+
+
+    let newItem = new item({
+        data: newItemData
+    })
+
+    newItem.save();
+
+}
+
+/*
 module.exports.login = async function(userUsername, userPassword){
     let access = false;
-  
-    // will wait before going down, else will return false as accces is false
-    await findAll().then(function(allData){
+    let no = 10;
 
-        allData.forEach(function(record){
-            if(record.Username === userUsername){
-                if(record.Password === userPassword){
-                    console.log("Accessed");
-                    access = true;
+    await user.findById(userUsername, 'Password', function (err, foundUser) {
 
-                    return; //going out of foreach
-                }
-                else{
-                    console.log("Incorrect Password");
-                }
+        if(foundUser === null){
+            console.log("User doesn't exist");
+            no= 12;
+
+        } else {
+            if(foundUser.Password === userPassword){
+                console.log("User Found");
+                no= 23;
+
             } else {
-                console.log("User doesnot exist");
+                console.log("Incorrect Password");
+                no= 33;
+
             }
-        })
-    })
+        }
+    });
+
+   return no;
+    // will wait before going down, else will return false as accces is false
+    // await findAll().then(function(allData){
+
+    //     allData.forEach(function(record){
+    //         if(access===true){
+    //             return; //going out of foreach
+    //         }
+
+    //         if(record.Username === userUsername){
+    //             if(record.Password === userPassword){
+    //                 console.log("Accessed");
+    //                 access = true;
+    //             }
+    //             else{
+    //                 console.log("Incorrect Password");
+    //             }
+    //         } else {
+    //             console.log("User doesnot exist");
+    //         }
+    //     })
+    // })
         
        
-        if(access==true){
-            console.log("true return");
-            return true;
+    //     if(access==true){
+    //         console.log("true return");
+    //         return true;
             
-        } else {
-            console.log("false return");
-            return false;
-        }
+    //     } else {
+    //         console.log("false return");
+    //         return false;
+    //     }
                 
-}
+} */
+
+// module.exports.login = async function(userUsername, userPassword){
+//     let access = false;
+  
+//     // will wait before going down, else will return false as accces is false
+//     await findAll().then(function(allData){
+
+//         allData.forEach(function(record){
+//             if(access===true){
+//                 return; //going out of foreach
+//             }
+
+//             if(record.Username === userUsername){
+//                 if(record.Password === userPassword){
+//                     console.log("Accessed");
+//                     access = true;
+//                 }
+//                 else{
+//                     console.log("Incorrect Password");
+//                 }
+//             } else {
+//                 console.log("User doesnot exist");
+//             }
+//         })
+//     })
+        
+       
+//         if(access==true){
+//             console.log("true return");
+//             return true;
+            
+//         } else {
+//             console.log("false return");
+//             return false;
+//         }
+                
+// }
