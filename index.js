748cf4eed3e4788afdde01062d993698b4c04314
Hassan <mr.und3ad1@gmail.com>
2023-03-10 12:56:10 +0500

    updated static path in server file

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.set("view engine", "ejs");
 app.set('views', __dirname + '/views');
 
 
-app.use(express.static('public'))
+app.use(express.static(__dirname))
 
 app.use(express.urlencoded({ extended: true }));
 
